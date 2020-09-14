f652ebd441e0f7ead5be200dcdf9c1e57028961e
sajith <sajith@digitalxlabs.com>
2020-09-14 18:50:25 +0530

    Improved biding algo

--- src/main/scala/com/example/Algorithm.scala
+++ src/main/scala/com/example/Algorithm.scala
@@ -1,28 +1,72 @@
 package com.example
 
-import java.util.UUID
-
 import com.example.CampaignsData.data
 
 object Algorithm {
 
   def bid(request: BidRequest): Option[BidResponse] = {
-    var res = data.filter(camp => camp.targeting.targetedSiteIds.contains(request.site.id))
+    var res = data.filter(camp => filterTargetSites(request, camp))
 
-    var filterUsersCountries: Seq[Campaign] = Seq()
-    if (request.user.isDefined && request.user.get.geo.isDefined && request.user.get.geo.get.country.isDefined) {
-      filterUsersCountries = data.filter(camp => camp.country.equalsIgnoreCase(request.user.get.geo.get.country.get))
-    }
-    if (filterUsersCountries.nonEmpty) {
-      res = res.intersect(filterUsersCountries)
-    }
 
-    val cmp = res.sortWith((l, r) => l.bid > r.bid).collectFirst(camp => camp)
-    if (cmp.isDefined) {
-      Option(BidResponse(UUID.randomUUID().toString, request.id, cmp.get.bid, Option(cmp.get.id.toString), cmp.get.banners.collectFirst(ba => ba)))
-    } else {
-      Option.empty
-    }
   }
 
+  private def filterBidRequest(request: BidRequest, campaign: Campaign): Boolean = {
+
+  }
+
+  private def filterTargetSites(request: BidRequest, campaign: Campaign): Boolean =
+    campaign.targeting.targetedSiteIds.contains(request.site.id)
+
+  private def filterUserCountry(request: BidRequest, campaign: Campaign): Boolean =
+    request.user match {
+      case None => true
+      case Some(user) => user.geo match {
+        case None => true
+        case Some(geo) => geo.country match {
+          case None => true
+          case Some(country) => campaign.country.equalsIgnoreCase(country)
+        }
+      }
+    }
+
+
+  private def filterDeviceCountry(request: BidRequest, campaign: Campaign): Boolean =
+    request.device match {
+      case None => true
+      case Some(device) => device.geo match {
+        case None => true
+        case Some(geo) => geo.country match {
+          case None => true
+          case Some(country) => campaign.country.equalsIgnoreCase(country)
+        }
+      }
+    }
+
+  private def filterBidFloor(request: BidRequest, campaign: Campaign): Boolean =
+    request.imp match {
+      case None => true
+      case Some(impressions) => impressions.exists(imp => imp.bidFloor match {
+        case None => true
+        case Some(bid) => campaign.bid >= bid
+      })
+    }
+
+
+  private def filterPlacementInfo(request: BidRequest, campaign: Campaign): Boolean = {
+    request.imp match {
+      case None => true
+      case Some(impressions) => impressions.exists(imp => {
+        campaign.banners.exists(banner => {
+          if (imp.w.isDefined && imp.h.isDefined && banner.height == imp.h.get && banner.width == imp.w.get) {
+            true
+          } else if (imp.hmax.isDefined && imp.hmin.isDefined &&) {
+            true
+          } else {
+            false
+          }
+        })
+      })
+    }
+  }
 }
+
